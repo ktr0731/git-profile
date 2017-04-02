9177be279bdd7774d801e9e53f1f7ef0ce27338b
lycoris0731 <aizu.s1230022@gmail.com>
2017-04-03 08:02:48 +0900

    [ADD] Implement add command

--- add.go
+++ add.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/mitchellh/cli"
+import (
+	"fmt"
+
+	"github.com/mitchellh/cli"
+)
 
 type AddCommand struct {
 	ui cli.Ui
@@ -11,9 +15,19 @@ func (c *AddCommand) Synopsis() string {
 }
 
 func (c *AddCommand) Help() string {
-	return "Usage: git profile add <title> <name> <email>"
+	return "Usage: git profile add [options] [<title> <name> <email>]"
 }
 
 func (c *AddCommand) Run(args []string) int {
+	if len(args) != 3 {
+		c.ui.Output(c.Help())
+		return 1
+	}
+
+	if err := NewProfile(args[0], args[1], args[2]).Save(); err != nil {
+		c.ui.Output(fmt.Sprintf("error in Profile.Save(): %s", err))
+		return 1
+	}
+
 	return 0
 }
